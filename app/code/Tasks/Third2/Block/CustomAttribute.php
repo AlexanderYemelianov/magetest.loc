c40309963b6243c6a904650c6875bc690ddfd72e
andriy <andriy.gavrysh@gmail.com>
2018-03-20 14:43:06 +0200

    third task with install data and new custom attr

--- app/code/Tasks/Third2/Block/CustomAttribute.php
+++ app/code/Tasks/Third2/Block/CustomAttribute.php
@@ -14,11 +14,29 @@ use Magento\Framework\View\Element\Template;
 class CustomAttribute extends \Magento\Framework\View\Element\Template
 {
 
+    /**
+     * @var
+     */
     protected $_customAttribute;
+    /**
+     * @var \Magento\Framework\Registry
+     */
     protected $_registry;
+    /**
+     * @var mixed
+     */
     protected $_product;
-    private $_attrName = 'sample_attribute';
+    /**
+     * @var string
+     */
+    private $_attrName = 'new_custom_attribute';
 
+    /**
+     * CustomAttribute constructor.
+     * @param Template\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param array $data
+     */
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
         \Magento\Framework\Registry $registry,
@@ -29,14 +47,23 @@ class CustomAttribute extends \Magento\Framework\View\Element\Template
         parent::__construct($context, $data);
     }
 
+    /**
+     * Get custom attribute from $_product by $_attrName
+     *
+     * @return \Magento\Framework\Phrase
+     */
     public function getCustomAttribute()
     {
-//        $product = $this->getCurrentProduct();
         $attr = $this->_product->getResource()->getAttribute($this->_attrName)->getFrontend()->getValue($this->_product);
 
         return $attr ?? __('Custom attribute not assigned.');
     }
 
+    /**
+     * Get custom attribute name from $_product by $_attrName
+     *
+     * @return mixed
+     */
     public function getCustomAttributeLabel()
     {
         $label = $address =$this->_product->getResource()->getAttribute($this->_attrName)->getStoreLabel();
@@ -44,6 +71,11 @@ class CustomAttribute extends \Magento\Framework\View\Element\Template
         return $label;
     }
 
+    /**
+     * Get $_product entity
+     *
+     * @return mixed
+     */
     public function getCurrentProduct()
     {
         return $this->_registry->registry('product');
